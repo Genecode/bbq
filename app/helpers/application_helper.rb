558f05c17c790db0f153bd7239b1eba7b7f20f24
Genecode <genemoose@mail.ru>
2018-05-23 16:05:47 +0300

    style devise form (registration, sign in), and error message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,24 @@
 module ApplicationHelper
   def user_avatar(user)
+    #TODO load users photo
     asset_path('user.png')
+  end
+
+  def devise_error_custom(resource)
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div id="error_explanation">
+      <p>#{sentence}</p>
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
   end
 end
